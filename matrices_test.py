3026e5acf43a652fba590b83aed0763e26691275
Ichiro Furusato <ichiro.furusato@gmail.com>
2021-03-26 21:32:59 +1300

    added preliminary matrices test

---
+++ matrices_test.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2020-2021 by Murray Altheim. All rights reserved. This file is part
+# of the Robot Operating System project, released under the MIT License. Please
+# see the LICENSE file included as part of this package.
+#
+# This tests the 11x7 Matrix, including several custom modes.
+#
+
+import pytest
+import sys, time
+from colorama import init, Fore, Style
+init()
+
+from lib.logger import Logger, Level
+from lib.i2c_scanner import I2CScanner
+from lib.enums import Orientation
+from lib.matrix import Matrices
+
+# ..............................................................................
+@pytest.mark.unit
+def test_matrix():
+
+    _log = Logger('matrix-test', Level.INFO)
+    _matrices = None
+
+    try:
+
+        _log.info(Fore.CYAN + 'start matrices test...')   
+        
+        _matrices = Matrices(Level.DEBUG)
+
+#       if _port_light:
+#           _port_light.text('Y', False, False)
+#       if _stbd_light:
+#           _stbd_light.text('N', False, False)
+#       time.sleep(2)
+
+#       if _port_light:
+#           _port_light.disable()
+#           _port_light.clear()
+#       if _stbd_light:
+#           _stbd_light.disable()
+#           _stbd_light.clear()
+#       time.sleep(1)
+
+        _log.info('matrix on...')   
+        _matrices.on()
+        time.sleep(2)
+
+        _log.info('matrix off...')   
+        _matrices.clear()
+        time.sleep(1)
+
+        _log.info('starting matrix blink_on...')   
+        _matrices.blink(True, 0.5)
+        time.sleep(1)
+        _matrices.blink(False, 0.5)
+        _matrices.clear()
+
+
+    except KeyboardInterrupt:
+        _log.info(Fore.MAGENTA + 'Ctrl-C caught: interrupted.')
+    finally:
+        _log.info('closing matrix test...')   
+        if _matrices:
+            _matrices.clear()
+
+
+# call main ......................................
+
+def main():
+
+    test_matrix()
+
+if __name__== "__main__":
+    main()
+
